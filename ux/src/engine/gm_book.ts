8da6f7649daea2a1059387a2cfca9f80cf26227c
Tapman104 <riddhishnknight@gmail.com>
2026-02-16 08:16:18 +0530

    feat: update package structure and configuration for improved build process and CI integration

--- ux/src/engine/gm_book.ts
+++ ux/src/engine/gm_book.ts
@@ -9,7 +9,7 @@ export const GM_BOOK: Record<string, { m: string, w: number }[]> = {
     ],
     // Response to e4
     'rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3': [
-        { m: 'e7e5', w: 100 }, { m: 'c7c5', w: 90 }, { m: 'e7e6', w: 50 }, { m: 'c7c6', w: 40 }
+        { m: 'e7e5', w: 100 }, { m: 'c7c5', w: 90 }, { m: 'e7e6', w: 50 }
     ],
     'rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq -': [{ m: 'g1f3', w: 100 }],
     'rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq -': [{ m: 'd7d6', w: 100 }, { m: 'e7e6', w: 80 }, { m: 'b8c6', w: 70 }],
