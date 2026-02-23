e49046a71961d55fadf3a67fd82f50a271dcf05b
Tapman104 <riddhishnknight@gmail.com>
2026-02-23 22:23:00 +0530

    fix(ux): resolve vite.config.ts TypeScript error to fix Vercel deploy

--- ux/vite.config.ts
+++ ux/vite.config.ts
@@ -5,8 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   worker: {
-    format: 'es',
-    plugins: []
+    format: 'es'
   },
   build: {
     target: 'es2020',
